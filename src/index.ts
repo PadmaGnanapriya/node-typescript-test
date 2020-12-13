1dd3e90797501b4d7bc998b2e9ee63d8854d90f1
PadmaGnanapriya <padmaisuru@gmail.com>
2020-12-13 17:07:15 +0530

    create index.ts file

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+// Create a new express app instance
+const app: express.Application = express();
+app.get('/', function (req, res) {
+res.send('Node Express TypeScript');
+});
+app.get('/comminglevel', function (req, res) {
+    res.send('Routing test for this');
+    });
+    
+
+const PORT :number=4000;
+app.listen(PORT, function () {
+console.log(`App is listening on port ${PORT}!`);
+});
